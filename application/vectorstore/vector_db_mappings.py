63dc500c43ebad36d4f8bed6edeca11178150ee8
Mitul Kalariya <mitul.k@simformsolutions.com>
2023-08-16 16:07:20 +0530

    removed: extra files

--- application/vectorstore/vector_db_mappings.py
+++ application/vectorstore/vector_db_mappings.py
@@ -1,11 +1,6 @@
-from vectorstore.custom_faiss import custom_faiss
-
-# from vectorstore.custom_pgvector import custom_pgvector
-from vectorstore.custom_pinecone import custom_pinecone, nbic_pinecone
+from vectorstore.custom_pinecone import nbic_pinecone
 
 
 VECTOR_DB_MAPPING = {
-    "faiss": custom_faiss,
-    # "pgvector": custom_pgvector,
     "pinecone": nbic_pinecone,
 }
